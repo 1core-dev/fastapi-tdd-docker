dd13fd90946ba4d58615737526d669e964d4212c
1core-dev <1core@mail.com>
2022-05-27 13:39:35 +0300

    Initial commit

---
+++ src/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Depends
+
+from app.config import Settings, get_settings
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {'Hello': 'root'}
+
+
+@app.get('/ping')
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        'ping': 'pong!',
+        'environment': settings.environment,
+        'testing': settings.testing,
+    }
